8ad0385e627a48423d5d1729448235e89bf4bb8b
avdata99 <andres@data99.com.ar>
2025-09-10 23:59:01 +0200

    Moving all files to allow working better

--- e2e/cypress/integration/resultados.js
+++
@@ -1,9 +0,0 @@
-describe('Pagina de resultados', () => {
-  beforeEach(() => cy.loginAsAdmin());
-
-  it('resultados', () => {
-    cy.visit('/elecciones/resultados');
-    cy.contains('Resultados para todo el país');
-  });
-
-});
